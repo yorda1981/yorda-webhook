510c8f7ee19f057ef772d1ea132ad3a389b9e39c
yorda1981 <yordasr@gmail.com>
2026-05-25 23:27:50 -0300

    Update redis.js

--- src/services/redis.js
+++ src/services/redis.js
@@ -1,2 +1,38 @@
-// Próxima etapa:
-// mover Redis completo aquí
+const Redis = require("ioredis");
+
+const {
+  REDIS_URL
+} = require("../config/env");
+
+let redis = null;
+
+try {
+
+  if (REDIS_URL) {
+
+    redis =
+      new Redis(
+        REDIS_URL
+      );
+
+    console.log(
+      "✅ Redis conectado"
+    );
+
+  } else {
+
+    console.log(
+      "⚠️ REDIS_URL no configurado"
+    );
+  }
+
+} catch (e) {
+
+  console.log(
+    "❌ Error Redis:",
+    e.message
+  );
+}
+
+module.exports =
+  redis;
