a29f8f20fb7494e2a9b6143c9246115d9794d0e1
yorda1981 <yordasr@gmail.com>
2026-05-25 23:32:17 -0300

    Update pricing-engine.js

--- src/engines/pricing-engine.js
+++ src/engines/pricing-engine.js
@@ -1,2 +1,74 @@
-// Motor comercial
-// Próxima etapa
+function calcularOperacion(
+  valor
+) {
+
+  valor =
+    Number(valor);
+
+  if (
+    isNaN(valor) ||
+    valor <= 0
+  ) {
+
+    return null;
+  }
+
+  // =====================
+  // TASAS
+  // =====================
+  let tasa = 100;
+
+  if (valor >= 100) {
+
+    tasa = 120;
+  }
+
+  if (valor >= 500) {
+
+    tasa = 122;
+  }
+
+  // =====================
+  // CALCULO
+  // =====================
+  const cup =
+    valor * tasa;
+
+  // =====================
+  // UPSELL
+  // =====================
+  let upsell = null;
+
+  if (valor < 100) {
+
+    const diferencia =
+      100 - valor;
+
+    const cupMejorado =
+      100 * 120;
+
+    upsell = {
+
+      falta:
+        diferencia,
+
+      nuevaTasa:
+        120,
+
+      nuevoTotal:
+        cupMejorado
+    };
+  }
+
+  return {
+
+    valor,
+    tasa,
+    cup,
+    upsell
+  };
+}
+
+module.exports = {
+  calcularOperacion
+};
