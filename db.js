6c938c6e6c3d06afb92d1033b4150ed0cbbd5154
yorda1981 <yordasr@gmail.com>
2026-05-30 23:13:41 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,18 @@
+const { Pool } = require("pg");
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.connect()
+  .then(() => {
+    console.log("✅ PostgreSQL conectado");
+  })
+  .catch(err => {
+    console.error("❌ Erro PostgreSQL:", err);
+  });
+
+module.exports = pool;
